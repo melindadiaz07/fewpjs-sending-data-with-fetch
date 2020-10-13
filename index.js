b0289145c32ccef02bf8bbcf340702259d2d70ed
Melindadiaz07 <melindadiaz@me.com>
2020-10-12 21:16:09 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your code here
+const submitData = (userName, userEmail) => {
+  let user = { name: `${userName}`, email: `${userEmail}`} 
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json",
+               "Accept": "application/json"},
+    body: JSON.stringify(user) 
+  })
+  .then(res => res.json())
+  .then(userObj => {
+    document.body. innerHTML = userObj["id"]
+  })
+  .catch(error => document.body.innerHTML = error.message)
+}
